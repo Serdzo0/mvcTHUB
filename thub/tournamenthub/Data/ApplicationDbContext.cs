c0d885ac7e247d550318adcf8ce7e93c14b257a3
Serdzo0 <sergej.ajlec@gmail.com>
2024-04-11 17:40:25 +0200

    Update .gitnore

---
+++ thub/tournamenthub/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tournamenthub.Models;
+
+namespace tournamenthub.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ thub/tournamenthub/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tournamenthub.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+
+    }
+}
